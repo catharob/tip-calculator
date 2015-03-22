b761564a86b9eaf7172b3dbb825301821c909e1d
Catherine Roberts <Catherine@Catherines-MacBook-Pro.local>
2015-03-22 18:53:36 -0400

    added Reset button

--- js/script.js
+++ js/script.js
@@ -14,3 +14,21 @@ function tipCalculate(){
 	var tipBox = document.getElementById('tipamount');
 	tipBox.innerHTML = '$' + tipTotal;
 }
+function resetCalculator(){
+	var totalBill = document.getElementById('bill').value;
+
+	var percBtns = document.getElementsByClassName('tip-btn');
+
+	for (var i = 0 ; i<percBtns.length; i++){
+		if(percBtns[i].checked){
+			percBtns[i].checked = false;
+		}
+	}
+
+	var tipBox = document.getElementById('tipamount');
+	tipBox.innerHTML = '$0.00';
+
+	var totalBill = document.getElementById('bill').value = 0;
+
+
+}
